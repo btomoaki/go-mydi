7802d377602c68f64d780871116a745e2ededbbc
root <root@LAPTOP-L4JC1K2I.localdomain>
2020-02-16 17:09:12 +0900

    add test and refactoring.

---
+++ domain/service/web/index_service.go
@@ -0,0 +1,47 @@
+//go:generate mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
+package web
+
+import (
+	"../../iface"
+
+	"github.com/gin-gonic/gin"
+)
+
+type IndexServiceInterface interface {
+	Get() func(*gin.Context)
+	NotAllowed() func(*gin.Context)
+	RenderIndex()
+}
+
+type IndexService struct {
+	Render iface.Render
+}
+
+func (s *IndexService) Get() func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		ctx.HTML(200, "index", gin.H{
+			"title":   "index",
+			"message": "hello world",
+		})
+	}
+}
+
+func (s *IndexService) NotAllowed() func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		ctx.HTML(405, "NotAllowed", gin.H{
+			"title":   "Method Not Allowed.",
+			"message": "Sorry... Method Not Allowed.",
+		})
+	}
+}
+
+func (s *IndexService) RenderIndex() {
+	s.Render.AddFromFiles("index",
+		"templates/common/base.html",
+		"templates/common/header.html",
+		"templates/index.html")
+	s.Render.AddFromFiles("NotAllowed",
+		"templates/common/base.html",
+		"templates/common/header.html",
+		"templates/index.html")
+}
---
+++ domain/service/web/index_service_test.go
@@ -0,0 +1,73 @@
+package web_test
+
+import (
+	reflect "reflect"
+
+	"../../iface"
+	"../web"
+	"github.com/gin-gonic/gin"
+	gomock "github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("IndexService", func() {
+	var (
+		c          *gomock.Controller
+		mockRender *iface.MockRender
+	)
+
+	Context("Get", func() {
+		BeforeEach(func() {
+			c = gomock.NewController(GinkgoT())
+			mockRender = iface.NewMockRender(c)
+		})
+		It("Call Get", func() {
+			indexService := &web.IndexService{Render: mockRender}
+			get := indexService.Get()
+			Expect(reflect.ValueOf(get).String()).To(Equal(reflect.ValueOf(func(ctx *gin.Context) {
+				ctx.HTML(200, "index", gin.H{
+					"title":   "index",
+					"message": "hello world",
+				})
+			}).String()))
+		})
+	})
+
+	Context("NotAllowed", func() {
+		BeforeEach(func() {
+			c = gomock.NewController(GinkgoT())
+			mockRender = iface.NewMockRender(c)
+		})
+		It("Call NotAllowed", func() {
+			indexService := &web.IndexService{Render: mockRender}
+			get := indexService.NotAllowed()
+			Expect(reflect.ValueOf(get).String()).To(Equal(reflect.ValueOf(func(ctx *gin.Context) {
+				ctx.HTML(405, "NotAllowed", gin.H{
+					"title":   "Method Not Allowed.",
+					"message": "Sorry... Method Not Allowed.",
+				})
+			}).String()))
+		})
+	})
+
+	Context("RenderIndex", func() {
+		BeforeEach(func() {
+			c = gomock.NewController(GinkgoT())
+			mockRender = iface.NewMockRender(c)
+		})
+		It("Call RenderIndex", func() {
+			indexService := &web.IndexService{Render: mockRender}
+			mockRender.EXPECT().AddFromFiles("index",
+				"templates/common/base.html",
+				"templates/common/header.html",
+				"templates/index.html")
+			mockRender.EXPECT().AddFromFiles("NotAllowed",
+				"templates/common/base.html",
+				"templates/common/header.html",
+				"templates/index.html")
+			indexService.RenderIndex()
+		})
+	})
+
+})
