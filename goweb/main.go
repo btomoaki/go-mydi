ed2c722851fa04a194c350d5c2ba43fdf3866f4b
root <root@LAPTOP-L4JC1K2I.localdomain>
2020-02-16 22:56:29 +0900

    use nginx.

--- goweb/main.go
+++ goweb/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"sync"
 
 	"github.com/btomoaki/go-mydi/di"
 )
 
 func main() {
 	Di := di.New()
+	wg := &sync.WaitGroup{}
 	contoror := Di.Contoror()
 	contoror.MapRoute()
 	contoror.RenderTemplate()
-	contoror.Run()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		contoror.Run()
+	}()
 	ioutil.WriteFile("/tmp/app-initialized", []byte("\n"), os.ModePerm)
+	wg.Wait()
 }
--- infra/router/router_repository.go
+++ infra/router/router_repository.go
@@ -1,10 +1,6 @@
 package router
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-
 	gin "github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/render"
 )
@@ -21,18 +17,7 @@ func NewRouter(render render.HTMLRender) *Router {
 }
 
 func (r *Router) Run(addr ...string) (err error) {
-	fmt.Println("###########fff#######################")
 	runerr := r.Gin.RunUnix("/tmp/nginx.socket")
-	fmt.Println("###########fff#######################")
-	if runerr == nil {
-		ioutil.WriteFile("/tmp/app-initialized", []byte("\n"), os.ModePerm)
-	}
-	stat, err := os.Stat("/tmp/app-initialized")
-	fmt.Println("##################################")
-	fmt.Println(stat)
-	fmt.Println(err)
-	fmt.Println(fmt.Sprintf("%#v,%#v", stat, err))
-	fmt.Println("##################################")
 	return runerr
 }
 
